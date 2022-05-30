5a3d9ef5edba64e0f2617462d8ac74986a34cd5f
Luis Vega <a11c12090@gmail.com>
2022-05-30 13:02:11 -0500

    Add FAQ page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './pages/faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'faq', component: FaqComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
