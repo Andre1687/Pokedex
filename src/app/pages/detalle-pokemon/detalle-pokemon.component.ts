46443137dcb059bcec4ea4a8c6caf707e3ddac4b
Andre Chavira <andrechavira1687@gmail.com>
2022-09-10 11:20:24 -0700

    Error detallePokemonService.ts

--- src/app/pages/detalle-pokemon/detalle-pokemon.component.ts
+++ src/app/pages/detalle-pokemon/detalle-pokemon.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DetallePokemonService } from './detalle-pokemon.service';
 
 @Component({
   selector: 'app-detalle-pokemon',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallePokemonComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private detallePokemonService: DetallePokemonService ) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe((params: any) =>  {
+        this.id = params.id;
+        console.log(this.id);
+        this.getDetallePokemon();
+      })
+  }
+
+  getDetallePokemon() {
+    this.detallePokemonService.getDetalle(this.id)
   }
 
 }
---
+++ src/app/pages/detalle-pokemon/detalle-pokemon.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetallePokemonService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+
+  getDetalle(id: string): Observable<any> {
+    return this.httpClient.get('https://pokeapi.co/api/v2/ability/' + id)
+      //.pipe(map((data: any) => {
+       // console.log(data);
+
+      //}))
+  }
+}
